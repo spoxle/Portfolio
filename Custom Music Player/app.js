d5823fe3fd08b3bfdb746e1ee24f6c015331411a
spoxle <colemaron@icloud.com>
2024-10-16 17:29:32 -0700

    created playlist library and fialized resizing

--- Custom Music Player/app.js
+++ Custom Music Player/app.js
@@ -1,14 +1,35 @@
 const root = document.querySelector(":root");
 
+/////////////
+// SIDEBAR //
+/////////////
+
 const sidebarResizer = document.getElementById("sidebar-resizer");
+const sidebarContent = document.getElementById("sidebar-content");
+const sidebarHeader = document.getElementById("sidebar-header");
 const sidebar = document.getElementById("sidebar");
 
-let currentValue = sidebar.clientWidth;
+function setSidebarWidth(n) {
+	root.style.setProperty("--sidebar-width", n + "px");
+
+	const sidebarWidth = sidebar.clientWidth;
+
+	if (sidebarWidth <= 75) {
+		sidebarHeader.style.display = "none";
+	} else {
+		sidebarHeader.style.display = "block";
+	}
+
+	root.style.setProperty("--playlists-columns", Math.ceil(sidebarWidth / 200))
+};
+
+// sidebar resizing
+
 let resizing = false;
 
 document.addEventListener("mousemove", (event) => {
 	if (resizing) {
-		root.style.setProperty("--sidebar-width", (event.clientX - sidebarResizer.clientWidth * 1.5) + "px");
+		setSidebarWidth(event.clientX - sidebarResizer.clientWidth * 1.5);
 	}
 });
 
@@ -16,6 +37,6 @@ sidebarResizer.addEventListener("mousedown", () => {
 	resizing = true;
 });
 
-document.addEventListener("mouseup", (event) => {
+document.addEventListener("mouseup", () => {
 	resizing = false;
 });
