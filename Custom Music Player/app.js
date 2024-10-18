cf3aa8b4472440299b3b29ebf3c5e600e0e79270
spoxle <colemaron@icloud.com>
2024-10-17 17:26:55 -0700

    worked on sidebar and playlists more

--- Custom Music Player/app.js
+++ Custom Music Player/app.js
@@ -1,4 +1,5 @@
 const root = document.querySelector(":root");
+const rootStyle = getComputedStyle(document.querySelector(":root"));
 
 /////////////
 // SIDEBAR //
@@ -9,17 +10,31 @@ const sidebarContent = document.getElementById("sidebar-content");
 const sidebarHeader = document.getElementById("sidebar-header");
 const sidebar = document.getElementById("sidebar");
 
+const playlistsInfo = document.querySelectorAll(".playlist > p");
+
+console.log(root.style.getPropertyValue("--container-padding"));
+
 function setSidebarWidth(n) {
 	const sidebarWidth = sidebar.clientWidth;
 
-	if (n <= 150) {
-		n = 75;
+	if (n <= 200) {
+		n = 100;
 	}
 
-	if (sidebarWidth <= 75) {
+	if (sidebarWidth <= 100) {
 		sidebarHeader.style.display = "none";
+		root.style.setProperty("--playlist-padding", 0);
+
+		playlistsInfo.forEach(function(info) {
+			info.classList.add("disabled")
+		});
 	} else {
 		sidebarHeader.style.display = "block";
+		root.style.setProperty("--playlist-padding", rootStyle.getPropertyValue("--container-padding"))
+
+		playlistsInfo.forEach(function(info) {
+			info.classList.remove("disabled")
+		});
 	}
 
 	root.style.setProperty("--sidebar-width", n + "px");
